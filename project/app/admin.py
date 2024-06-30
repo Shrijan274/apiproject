5f4f9e67990145205716c0acc5a418e3153fa7c1
Shrijan274 <shrijanlingam@gmail.com>
2024-06-30 21:32:03 +0530

    structred admin.py for reflection

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+# Define a custom UserAdmin
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'first_name', 'last_name', 'date_joined', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'username')
+    readonly_fields = ('date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+# Register your CustomUser model with the custom UserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
