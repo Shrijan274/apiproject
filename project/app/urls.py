b61893956de62cd75e74e0b830959fc99d50fef4
Shrijan274 <shrijan.lingam@clientservertech.com>
2024-07-01 19:50:27 +0530

    added request examples

--- project/app/urls.py
+++ project/app/urls.py
@@ -6,3 +6,20 @@ urlpatterns = [
     path('api/login/', views.login_view, name='login'),
     path('api/logout/', views.logout_view, name='logout'),
 ]
+
+
+#http://127.0.0.1:8000/api/signup/
+#{
+#   "email":"demo@gmail.com",
+#   "username":"demo",
+#   "first_name":"abc",
+#   "last_name":"def",
+#   "password":"demo"
+#}
+
+
+# http://127.0.0.1:8000/api/login/
+# {
+#     "email":"demo@gmail.com",
+#     "password":"demo"
+# }
