1934e825e174049fa6bef37a6f7c5920dcb95bd3
Shrijan274 <shrijanlingam@gmail.com>
2024-07-03 15:44:17 +0530

    validating at api level

--- project/app/serializers.py
+++ project/app/serializers.py
@@ -2,11 +2,33 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        email=data.get('email')
+        username=data.get('username')
+        first_name=data.get('first_name')
+        last_name=data.get('last_name')
+        
+        valid_email=('gmail.com','yahoo.com','microsoft.com')
+        invalid_names=['baby','sweety','cutie','bullshit']
+        
+        if not any(email.endswith(domain) for domain in valid_email):
+            raise serializers.ValidationError('Invalid email domain')
+        if any(invalid_name in username for invalid_name in invalid_names):
+            raise serializers.ValidationError('Invalid name in username')
+        if any(invalid_name in first_name for invalid_name in invalid_names):
+            raise serializers.ValidationError('Invalid name in first name')
+        if any(invalid_name in last_name for invalid_name in invalid_names):
+            raise serializers.ValidationError('Invalid name in last name')
+        
+        return data    
+    
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
     class Meta:
         model = CustomUser
         fields = ['email', 'username', 'first_name', 'last_name', 'password']
         extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
-        return user
+    
+    
